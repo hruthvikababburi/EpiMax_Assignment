9a31bb11797b08a7a4391f46047cbb1f6b065556
Hruthvika Babburi <hruthvikababburi@gmail.com>
2024-05-01 17:51:02 +0530

    installed mongoose and connected successfully to mongoDB, installed dotenv to maintain security of the credentials and added it to gitignore.

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,40 @@
 const express = require('express')
 const cors = require('cors')
-
+const mongoose = require('mongoose')
 const app = express()
 
+require('dotenv').config()
+
 app.use(cors())
+app.use(express.json())
+
+const mongodb_username = process.env.MONGODB_USERNAME
+const mongodb_password = process.env.MONGODB_PASSWORD
+
+mongoose.connect(`mongodb+srv://${mongodb_username}:${mongodb_password}@taskmanager.e9veqsx.mongodb.net/?retryWrites=true&w=majority&appName=TaskManager`)
+
+const db = mongoose.connection;
+
+db.on('error',(err)=>{
+    console.log('MongoDB Connection Error', err);
+})
+
+db.once('open',()=>{
+    console.log('Connected to MongoDB')
+})
+
+db.on('disconnected',()=>{
+    console.log('MongoDB Disconnected')
+})
 
 const PORT = process.env.PORT || 3000
+
+app.get('/',(req,res)=>{
+    res.send('HELOO USER')
+})
+
+
+// mongodb+srv://hruthvikababburi:<password>@taskmanager.e9veqsx.mongodb.net/?retryWrites=true&w=majority&appName=TaskManager
+
 
 app.listen(PORT,()=>{console.log(`Running Server at ${PORT}`)})
